2cdf314aae786d4453d28ec7821943f3256bf9fa
ElonaHoxhaj96 <elona6915@gmail.com>
2024-09-11 17:55:49 +0200

    fixed DB_Database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Movie;
 class PageController extends Controller
 {
      // il petodo index è il metodo che si usa per la home
      public function index(){
+        $movies = Movie::all();
+        dd($movies);
         return view('home');
     }
    
