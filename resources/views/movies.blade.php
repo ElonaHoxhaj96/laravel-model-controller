a6d9576c2178572d4e306dbdda32ab105f0faa2f
ElonaHoxhaj96 <elona6915@gmail.com>
2024-09-12 10:26:30 +0200

    fixed style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,35 +2,10 @@
 @extends('layouts.main')
 
 @section('content')
-<!-- <div class="container my-5">
-    <h1>Film</h1>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Titolo</th>
-      <th scope="col">Titolo originale</th>
-      <th scope="col">Paese</th>
-      <th scope="col">Data</th>
-      <th scope="col">Voto/th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($movies as $movie)
-        <tr>
-        <td>{{$movie->id}}</td>
-        <td>{{$movie->title}}</td>
-        <td>{{$movie->original_title}}</td>
-        <td>{{$movie->nationality}}</td>
-        <td>{{$movie->date}}</td>
-        <td>{{$movie->vote}}</td>
-        </tr>
-    @endforeach
-  </tbody>
-</table> -->
-<div class="container">
+
+<div class="container d-flex wrap">
   @foreach($movies as $movie)
-    <div class="card" style="width: 18rem;">
+    <div class="card mb-2 mt-2" style="width: 18rem;">
       <div class="card-header">
         <h3>Titolo: {{$movie->title}}</h3>
       </div>
